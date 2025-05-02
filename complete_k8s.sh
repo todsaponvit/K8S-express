9a11cd2de96aee5cfae978e46f7d063cb3984004
Todsapon Vittayasakoonchai <65076408+todsaponvit@users.noreply.github.com>
2025-05-02 19:27:11 +0700

    Create complete_k8s.sh

---
+++ complete_k8s.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+chmod +x kubectl
+mkdir -p ~/.local/bin
+mv ./kubectl ~/.local/bin/kubectl
+kubectl version --client
+echo "1/3 Install K8S Successfull"
